2d6ebeff98278521eb835904aea50ff404df0740
Muhammad Umair <umairinam76@gmail.com>
2025-04-09 22:05:58 +0500

    Initial commit - StartHub Academy test task

---
+++ src/app/user-list/page.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import users from '@/data/users.json';
+import './page.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'User List | StartHub Academy',
+  description: 'Browse our team of expert engineers and developers.',
+};
+
+export default function UserListPage() {
+  return (
+    <main>
+      <h1 tabIndex={0}>User List</h1>
+      <ul aria-label="User List">
+        {users.map((user) => (
+          <li key={user.id}>
+            <article>
+              <div className="user-card">
+                <Image
+                  src={user.avatar}
+                  alt={`Avatar of ${user.name}`}
+                  width={64}
+                  height={64}
+                  className="avatar"
+                />
+                <div className="user-info">
+                  <h2 tabIndex={0}>{user.name}</h2>
+                  <p>{user.description}</p>
+                </div>
+              </div>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
